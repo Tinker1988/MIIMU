41b889217d75d048d800df96483cc8f85d4b953e
Khushi Singh <ksingh.172005@gmail.com>
2026-03-31 16:14:51 +0530

    fix: removed unused useLocation

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,4 +1,4 @@
-import { Link, useLocation, useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { Button } from './ui/button'
 import { useUser } from '@/context/UserContext'
 
